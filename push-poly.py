233e64fc14db7f4c3b7c43993d59e981d70a5a46
markv58 <markvittes@gmail.com>
2019-05-24 18:45:10 -0500

    Update push-poly.py

--- push-poly.py
+++ push-poly.py
@@ -51,6 +51,7 @@ class Controller(polyinterface.Controller):
         LOGGER.info('Started Push Nodeserver')
         self.check_params()
         self.setDriver('ST', 1)
+	LOGGER.info('If you have just upgraded hit the Update Profile button and restart the Admin Console')
         
     def shortPoll(self):
         pass
