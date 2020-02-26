f4ff102bd19244706f516e30c5f6d455ae0a6b72
markv58 <markvittes@gmail.com>
2020-02-26 09:01:13 -0600

    Update push-poly.py

--- push-poly.py
+++ push-poly.py
@@ -92,11 +92,12 @@ class Controller(polyinterface.Controller):
                 _val = key.lower()
                 _cleanaddress = _val.replace(' ','')
                 if val == "":
-                    _address = _cleanaddress[:15]
+                    _address = (_cleanaddress[:12] + _cleanaddress[-3:])
+                    LOGGER.debug(str(_address))
                 else:
                     address = val.lower()
                     _clnaddress = address.replace(' ','')
-                    _address = _clnaddress[:15]
+                    _address1 = _clnaddress[:12] + _clnaddress[3:]
                 _key = key
                 self.addNode(thingnode(self, self.address, _address, _key))
 		
